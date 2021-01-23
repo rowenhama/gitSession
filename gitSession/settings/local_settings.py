ccf6e4774504936aa366c51fb04bff5c02f2ca60
taoneswa <61433155+taoneswa@users.noreply.github.com>
2021-01-23 19:34:17 +0200

    Create local_settings.py

---
+++ gitSession/settings/local_settings.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'postgres',
+        'PASSWORD': 'icui4cu2',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
