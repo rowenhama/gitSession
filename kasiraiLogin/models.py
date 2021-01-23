a630dac47fc04ce327f86e8c0b58e125e25d6c75
taoneswa <61433155+taoneswa@users.noreply.github.com>
2021-01-23 19:16:29 +0200

    Update models.py

--- kasiraiLogin/models.py
+++ kasiraiLogin/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Registration(models.Model):
+    username = models.CharField(max_length=50)
+    fullname = models.CharField(max_length=50)
+    date_of_birth = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
